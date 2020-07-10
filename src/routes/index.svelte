621f5e83055fc633df1419495b4a1ced45b16c26
Gabriel Gianordoli <gabriel.gianordoli@nytimes.com>
2020-07-09 22:22:35 -0400

    lots of stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,69 +1,52 @@
 <script>
+	import _ from 'lodash';
 	import 'lazysizes';
 	import images from '../../data/imagedata.json';
+	import { work } from '../../data/doc.json';
 
 	const imagesPath = 'images/';
-	images.map(i => {
-		const { slug, sizes, extension, hasRetina } = i;
-		i.srcset = sizes.reduce((acc, curr, index, array) => {
+	images.map(img => {
+		const { slug, sizes, extension, hasRetina } = img;
+		img.srcset = sizes.reduce((acc, curr, index, array) => {
 			acc += `${imagesPath}${slug}-${curr}.${extension} ${curr}w`;
-			if (hasRetina) acc += `, ${imagesPath}${slug}-${curr}_x2.${extension} ${curr*2}w`;
+			if (hasRetina) acc += `, ${imagesPath}${slug}-${curr}_x2.${extension} ${curr * 2}w`;
 			if (index < array.length - 1) acc += ',';
 			return acc;
 		}, '');
-		return i;
-	});
+		img.src = `${imagesPath}${slug}-${sizes[Math.ceil(sizes.length/2)]}.${extension}`;
+		return img;
+	});	
+
+	work.filter(item => item.type === 'image')
+		.map(item => processImage(item));
+
+	function getImageData(slug) {
+		return images.find(i => i.slug === slug);
+	}
+
+	function processImage(item) {
+		const slug = item.value.slug.substring(0, item.value.slug.indexOf('.'));
+		const imageData = getImageData(slug);
+		if (imageData) item.value = Object.assign(item.value, imageData);
+	}	
+
 </script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Luciano Feijão</title>
 </svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
-{#each images as { slug, sizes, extension, srcset }}
-<img alt="" data-sizes="auto" data-srcset="{srcset}" class="big lazyload"/>
+{#each work as { type, value }}
+	<img alt="{value.caption}" data-sizes="auto" data-src="{value.src}" data-srcset="{value.srcset}" class="big lazyload"/>
+	<p>{value.caption}</p>
 {/each}
 
 <style>
-	h1,
-	figure,
-	p {
-		text-align: center;
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
 
 	img {
 		width: 100%;
 		max-width: 400px;
 		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
 	}
 
 	img.big {
